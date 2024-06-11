944679f7066cfec4966844ec8f64d00b4e803e89
a23juanprv <Juanparovas3@gmail.com>
2024-06-11 10:55:41 +0200

    test (ServiceTest): add tests

--- src/test/java/edu/badpals/domain/FruitIT.java
+++
@@ -1,8 +0,0 @@
-package edu.badpals.domain;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class FruitIT extends FruitTest {
-    // Execute the same tests but in packaged mode.
-}
