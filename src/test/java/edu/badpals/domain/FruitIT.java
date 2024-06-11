4cc99e505606a6664a2164c26a09d9ae76b6ae73
a23juanprv <Juanparovas3@gmail.com>
2024-06-11 10:41:32 +0200

    build(farmersAndFruits): create base project

---
+++ src/test/java/edu/badpals/domain/FruitIT.java
@@ -0,0 +1,8 @@
+package edu.badpals.domain;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class FruitIT extends FruitTest {
+    // Execute the same tests but in packaged mode.
+}
