de21d8d338271ee8c766921ce585dc656519cfc3
a23juanprv <Juanparovas3@gmail.com>
2024-06-11 12:32:48 +0200

    feat (ServiceFruit.java): add method add

--- src/main/java/edu/badpals/service/ServiceFruit.java
+++ src/main/java/edu/badpals/service/ServiceFruit.java
@@ -2,10 +2,12 @@ package edu.badpals.service;
 
 
 
+import edu.badpals.domain.Farmer;
 import edu.badpals.domain.Fruit;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.List;
+import java.util.Optional;
 
 @ApplicationScoped
 public class ServiceFruit {
@@ -15,5 +17,16 @@ public class ServiceFruit {
         List<Fruit> fruitList = Fruit.listAll();
         return fruitList;
     }
+
+    public void add(Fruit fruit){
+
+        Optional<Farmer> farmer = Farmer.find("name = ?1", fruit.getFarmer().getName()).firstResultOptional();
+
+        Fruit fruta = new Fruit(fruit.getName(), fruit.getDescription(), farmer.get());
+
+        fruta.persist();
+
+    }
+
 
 }
